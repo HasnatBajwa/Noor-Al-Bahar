f8cbd275684a7492a690a725456682dff3350b1b
Hasnat Bajwa <hasnatjbajwa@gmail.com>
2022-09-15 11:06:48 -0700

    Created Contact Us Page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,13 +1,55 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import Header from '../../Components/Header/Header';
 import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
-const Contact = () =>{
-    return(
+import './Contact.css';
+import { useState } from 'react';
+import whatsapp from '../../Assets/whatsapp.png';
+const Contact = () => {
+    const [name, nameHandler] = useState('');
+    function submit() {
+        if (name == '') {
+            alert("Please Enter Your Name");
+        }
+        else
+            alert(`Welcome ${name} Our Representative Will Get In Touch With You Soon`);
+
+    }
+    return (
         <>
-        <Header/>
-        <Navbar/>
-        contact page
-        <Footer/>
+            <Header />
+            <Navbar />
+            <div className="container-fluid" id='contact'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-xs-12 userinput">
+                            <h2 className='h2'>We're Ready to Cater All Your Queries</h2>
+                            <input className='input' type="text" id='name' placeholder='Your Name' onChange={(e) => { nameHandler(e.target.value) }} /> <br />
+                            <input className='input' type="text" placeholder='Email Address' /><br />
+                            <input className='input' type="text" placeholder='Phone Number' />
+                            <button id='btn' onClick={submit}>Submit</button>
+                        </div>
+                        <div className="col-md-6 col-xs-12 userinput">
+                            <h2 className="h2">Our Contacts</h2>
+                            <p><b>Email: </b>nooralbahar.shj@gmail.com</p>
+                            <p><b>Phone: </b>+971-50-8988451 | +971-50-6777487</p>
+                            <p><b> Or Whatsapp us By Clicking Here !</b></p>
+                            <img src={whatsapp} alt="whatsapp" />
+                        </div>
+                    </div>
+                </div>
+                <div className="row maps">
+                    <div className="col-md-12 col-xs-12">
+                        <h2 >
+                            Our Location
+                        </h2>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2517.463610125323!2d55.420966522346276!3d25.31079603316641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2069051ca46f19ed!2zMjXCsDE4JzM3LjQiTiA1NcKwMjUnMTYuMCJF!5e0!3m2!1sen!2s!4v1663176733027!5m2!1sen!2s"  referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+
+            <Footer />
         </>
     );
 }
